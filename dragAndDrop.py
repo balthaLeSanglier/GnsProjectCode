13e9f94b4d2c66f11680a86be791de86d840b6f3
Balthazar <balthazar.muhlstein@etu.univ-lyon1.fr>
2025-01-30 08:52:15 +0100

    Table de correspondance

---
+++ dragAndDrop.py
@@ -0,0 +1,18 @@
+dicoCorrespondance = {}
+
+dicoCorrespondance = {
+  "R11": "0cd5e6e8-a314-4976-9af0-73cee44fe070",
+  "R12": "805e7a1b-26e1-4e17-bde0-08089b23471b",
+  "R13": "63a70cc7-808d-44bf-9474-dcc3db5fba1d",
+  "R14": "250ff1dd-e98e-4993-a470-26f8e4764d19",
+  "R15": "dd178519-b796-4ab6-9650-05224036e5c7",
+  "R16": "af01ad7b-85a9-4d4c-8aeb-06dd90076d06",
+  "R17": "dcb179f0-d0eb-46d6-a640-d62464aaef15",
+  "R21": "e6389003-fc98-442b-86a9-863d32cd7748",
+  "R22": "8e0c8e39-237e-41b1-bbfe-577c0ab91c7b",
+  "R23": "32d9435a-d76e-4f14-b795-b2a0f8f901ff",
+  "R24": "aba60752-e158-4541-bd31-1fea77dd93b4",
+  "R25": "264d0e64-32f6-4c1d-8407-e36e5835d9fc",
+  "R26": "UNKNOWN",
+  "R27": "7b6bdd45-c3fc-4ae1-b1f9-3b8f51e3af9e",
+}
